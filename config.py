3ee6d36e7e92e622bf8077d500bca38728acae6f
Avet <crykrypta@gmail.com>
2024-09-30 20:01:12 +0300

    update config.py

--- config.py
+++ config.py
@@ -5,9 +5,11 @@ from dataclasses import dataclass
 @dataclass
 class Config:
     tg_token: str
+    db_url: str
 
 
 def load_config() -> Config:
     env = Env()
     env.read_env()
-    return Config(tg_token=env.str('TG_API_KEY'))
+    return Config(tg_token=env.str('TG_API_KEY'),
+                  db_url=env.str('DATABASE_URL'))
