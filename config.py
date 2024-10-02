6b9707371bd0fc32d9a46ed9c22d891a22ca6344
Avet <crykrypta@gmail.com>
2024-10-02 11:56:15 +0300

    update config loading from env. added openai_key

--- config.py
+++ config.py
@@ -6,10 +6,12 @@ from dataclasses import dataclass
 class Config:
     tg_token: str
     db_url: str
+    openai_key: str
 
 
 def load_config() -> Config:
     env = Env()
     env.read_env()
     return Config(tg_token=env.str('TG_API_KEY'),
-                  db_url=env.str('DATABASE_URL'))
+                  db_url=env.str('DATABASE_URL'),
+                  openai_key=env.str('OPENAI_API_KEY'))
