c7db45f0b9f66384f7d0527fc000bbb2699ffb77
Avet <crykrypta@gmail.com>
2024-10-08 16:54:22 +0300

    added logs to config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
+import logging
 from environs import Env
 from dataclasses import dataclass
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -10,8 +13,12 @@ class Config:
 
 
 def load_config() -> Config:
+    logger.info('Loading configuration...')
     env = Env()
     env.read_env()
     return Config(tg_token=env.str('TG_API_KEY'),
                   db_url=env.str('DATABASE_URL'),
                   openai_key=env.str('OPENAI_API_KEY'))
+
+
+logger.info('Configuration loaded!')
