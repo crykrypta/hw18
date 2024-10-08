c50dce9996cd370d8bd79a9ab6f6e52ed0d40962
Avet <crykrypta@gmail.com>
2024-10-08 16:57:33 +0300

    add feauture: now for start chatting with bot, u need to push the button ST ART

--- keyboards.py
+++ keyboards.py
@@ -43,7 +43,7 @@ def get_main_keyboard(language: str) -> InlineKeyboardMarkup:
     Args:
         language (str): Язык 'ru' | 'eng'
     Returns:
-        InlineKeyboardMarkup: Клавиатура
+        InlineKeyboardMarkup: Клавиатура ((help, ch_lang), (chat_start)) # noqa
     """
     main_keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
@@ -53,10 +53,35 @@ def get_main_keyboard(language: str) -> InlineKeyboardMarkup:
              InlineKeyboardButton(
                  text=lexicon[language]['buttons']['ch_lang'],
                  callback_data='ch_lang')],
+            [InlineKeyboardButton(
+                text=lexicon[language]['buttons']['start_chat'],
+                callback_data='start_chat')]
+        ]
+    )
+    return main_keyboard
+
+
+# Inline клавиатура для главного меню
+def chat_keyboard(language: str) -> InlineKeyboardMarkup:
+    """
+    Args:
+        language (str): Язык 'ru' | 'eng'
+        in_chat (bool): Находится ли пользователь в чате
+    Returns:
+        InlineKeyboardMarkup: Клавиатура ((help, ch_lang), (reset_requests, chat_stop)) # noqa
+    """
+    main_keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(
+                text=lexicon[language]['buttons']['help'],
+                callback_data='help'),
+             InlineKeyboardButton(
+                text=lexicon[language]['buttons']['stop_chat'],
+                callback_data='chat_stop')],
 
             [InlineKeyboardButton(
                 text=lexicon[language]['buttons']['reset_requests'],
-                callback_data='reset_requests')]
+                callback_data='reset_requests')],
         ]
     )
     return main_keyboard
