7f4b54d0db0d914861e1669571727bcff43f05a0
Avet <crykrypta@gmail.com>
2024-10-08 16:58:12 +0300

    update lexicon

--- lexicon.py
+++ lexicon.py
@@ -1,4 +1,3 @@
-
 # Лексикон на русском языке
 lexicon = {
     'ru': {
@@ -12,14 +11,27 @@ lexicon = {
         },
         'text': {
             'any_text': 'Текстовое сообщение получено!',
-            'welcome': 'Привет! Вот список достуупных команд:',
+            'welcome': 'Добро пожаловать!',
         },
         'buttons': {
             'help': 'Помощь',
+            'ch_lang': 'Сменить язык',
+            'reset_requests': 'Сбросить запросы',
+            'start_chat': 'Начать чат',
+            'stop_chat': 'Выход',
         },
         'types': {
             'voice': 'Голосовое сообщение получено!',
             'photo': 'Фотография сохранена',
+        },
+        'error': {
+            'user_not_found': 'Пользователь не найден, введите команду /start',
+            'requests_limit': 'Превышен лимит запросов, купите еще',
+        },
+        'actions': {
+            'reset_requests': 'Запросы сброшены',
+            'chat_start': 'Чат запущен!',
+            'chat_stop': 'Чат остановлен',
         }
     },
     # Лексикон на английском языке
@@ -37,11 +49,24 @@ lexicon = {
             'welcome': 'Hello! Here is the list of available commands:',
         },
         'buttons': {
-            'help': 'Help'
+            'help': 'Help',
+            'ch_lang': 'Change language',
+            'reset_requests': 'Reset requests',
+            'start_chat': 'Start Chat',
+            'stop_chat': 'Exit',
         },
         'types': {
             'voice': 'We’ve received a voice message from you!',
             'photo': 'Photo saved!'
+        },
+        'error': {
+            'user_not_found': 'User not found, enter the command /start',
+            'limit': 'Request limit exceeded, but you can buy more',
+        },
+        'actions': {
+            'reset_requests': 'Requests reseted',
+            'chat_start': 'Chat started, just start typing!', # noqa
+            'chat_stop': 'Chat stopped',
         }
     }
 }
